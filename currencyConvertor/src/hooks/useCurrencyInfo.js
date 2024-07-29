8f6807876fc46f28b5b8daf8734c03abca6bf1bc
RohitAmbulge <rohitambulge15@gmail.com>
2024-07-29 23:21:37 +0530

    Currency Convertor Project Completed

--- currencyConvertor/src/hooks/useCurrencyInfo.js
+++ currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -3,9 +3,9 @@ import {useEffect , useState} from 'react'
 function useCurrencyInfo(currency){
     const [data, setData]  = useState({})
     useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
-        .then(() =>res.json()) //converted res into json
-        .then((res) => res[currency]) //we get the same value of key that was we passed in function as a value of currency
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+        .then((res) =>res.json()) //converted res into json
+        .then((res) => setData(res[currency])) //we get the same value of key that was we passed in function as a value of currency
         console.log(data);
     }, [currency])
 
